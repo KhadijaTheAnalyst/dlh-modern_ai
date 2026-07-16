13a620e9dea5ee819206eb5d90ae28e1b5fadc7a
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-07-16 11:40:21 +0200

    fixing errors

--- data_analysis/web_scraping/6-products_list.py
+++ data_analysis/web_scraping/6-products_list.py
@@ -16,7 +16,7 @@ def scrape_products(url):
             {"title": str, "price": str, "description": str, "rating": int}
     """
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
