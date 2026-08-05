c64dc39f437bf574812f78125d160af55e3b1364
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-08-05 15:10:37 +0200

    adding a new module

---
+++ deep_learning/dl_with_keras/0-sequential.py
@@ -0,0 +1,21 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def build_model(input_dim, neurons_h):
+    """
+    Builds a shallow neural network for multi-class classification
+    using the Keras Sequential API.
+
+    Arguments:
+        input_dim: Number of input features.
+        neurons_h: Number of neurons for the hidden layer.
+
+    Returns:
+        model: Keras model.
+    """
+    model = keras.Sequential([
+        layers.Dense(neurons_h, activation='sigmoid', input_shape=(input_dim,)),
+        layers.Dense(10, activation='softmax')
+    ])
+    return model
