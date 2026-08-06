88bbb31727e3966028677dff50abdf06e5a44397
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-08-06 14:49:31 +0200

    adding a new module

---
+++ deep_learning/dl_with_keras/5-save_load_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Save and load a full Keras model (architecture, weights, optimizer state)."""
+from tensorflow import keras
+
+
+def save_model(model, filepath):
+    """
+    Saves a trained Keras model to the specified filepath.
+
+    Arguments:
+        model: A trained Keras model to be saved.
+        filepath: Path (including file name) where the model will be saved.
+
+    Returns:
+        None.
+    """
+    model.save(filepath)
+    return None
+
+
+def load_model(filepath):
+    """
+    Loads a full Keras model from the specified filepath.
+
+    Arguments:
+        filepath: Path (including file name) from where the model
+                  will be loaded.
+
+    Returns:
+        model: The reloaded Keras model.
+    """
+    model = keras.models.load_model(filepath)
+    return model
