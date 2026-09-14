1a07d776b74e39a8c318aa53c23da7b6216e1737
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-09-14 13:37:31 +0200

    fixing Error

--- deep_learning/cv_apps/3-train_aug.py
+++ deep_learning/cv_apps/3-train_aug.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from ultralytics import YOLO
 
-def train_with_augmentation(data_yaml, model="yolov8n.pt", aug=None, custom_albu=None,
+def train_with_augmentation(data, model="yolov8n.pt", aug=None, custom_albu=None,
                            epochs=50, imgsz=640, batch=16, save=True, plots=True, verbose=False):
     model_obj = YOLO(model)
 
     train_config = {
-        'data': data_yaml,
+        'data': data,
         'epochs': epochs,
         'imgsz': imgsz,
         'batch': batch,
