1a9b0237b1ebf4812a9cceaba6eb4142a402b20f
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-09-14 11:53:49 +0200

    fixing Error

--- deep_learning/cv_apps/1-basic_aug.py
+++ deep_learning/cv_apps/1-basic_aug.py
@@ -1,28 +1,55 @@
 #!/usr/bin/env python3
+
 import albumentations as A
 import numpy as np
 
+
 def basic_aug(image, bboxes, labels):
-    transform = A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.RandomBrightnessContrast(
-            brightness_limit=0.2,
-            contrast_limit=0.2,
-            p=0.2
+    """
+    Apply basic data augmentation to an image and its bounding boxes.
+
+    Args:
+        image: Input image.
+        bboxes: Bounding boxes in Pascal VOC format.
+        labels: Labels corresponding to the bounding boxes.
+
+    Returns:
+        Augmented image, augmented bounding boxes, and augmented labels.
+    """
+
+    transform = A.Compose(
+        [
+            A.HorizontalFlip(p=0.5),
+
+            A.RandomBrightnessContrast(
+                brightness_limit=0.2,
+                contrast_limit=0.2,
+                p=0.2
+            ),
+
+            A.Affine(
+                translate_percent=(-0.1, 0.1),
+                scale=(0.9, 1.1),
+                rotate=(-30, 0),
+                p=0.5,
+                border_mode=0
+            )
+        ],
+        bbox_params=A.BboxParams(
+            format="pascal_voc",
+            label_fields=["class_labels"]
         ),
-        A.Affine(
-            translate_percent=(-0.1, 0.1),
-            scale=(0.9, 1.1),
-            rotate=(-30, 0),
-            p=0.5
-        )
-    ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']),
-       seed=42)
+        seed=42
+    )
 
-    transformed = transform(image=image, bboxes=bboxes, class_labels=labels)
+    transformed = transform(
+        image=image,
+        bboxes=bboxes,
+        class_labels=labels
+    )
 
-    augmented_image = transformed['image']
-    augmented_bboxes = np.array(transformed['bboxes'])
-    augmented_labels = transformed['class_labels']
+    augmented_image = transformed["image"]
+    augmented_bboxes = np.array(transformed["bboxes"])
+    augmented_labels = transformed["class_labels"]
 
     return augmented_image, augmented_bboxes, augmented_labels
