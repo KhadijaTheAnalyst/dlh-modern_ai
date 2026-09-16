b15b787a81c8eda6fb960af4868785cf1fd70532
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-09-16 15:47:49 +0200

    adding new module

--- data_analysis/intro_to_nlp/1-clean_text.py
+++ data_analysis/intro_to_nlp/1-clean_text.py
@@ -76,6 +76,8 @@ def clean_text(text, replace_num=True,
         text = emoji.replace_emoji(text, replace='<EMO>')
     elif emoji_action == "remove":
         text = emoji.replace_emoji(text, replace=' ')
+    elif emoji_action == "keep":
+        pass
     # "keep" does nothing
 
     # 7. Collapse repeated ! or ?
